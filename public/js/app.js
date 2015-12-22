7bc4cdfd1f68c7b190df500ad764ec8e8b66c5e9
Ray Masaki <ray@raymasaki.com>
2015-12-22 16:59:55 -0500

    probably going to start over

--- public/js/app.js
+++ public/js/app.js
@@ -12,10 +12,26 @@ function MainRouter($stateProvider, $urlRouterProvider) {
                 return '/work/' + $stateParams.projectId + '.html';
             },
             controller: function($stateParams){
+               angular.element('.container').velocity({
+                  top: '56px'
+               },{
+                  duration: 600,
+                  easing: [0.34, 0.34, 0.25, 1]
+               });
+
+               // $("html, body").animate({ scrollTop: 0 }, "fast");
+               // document.body.scrollTop = 0;
                window.setTimeout(function() {
                   var projectClass = '.' + $stateParams.projectId;
                   angular.element(projectClass).addClass('selected');
-               }, 300);
+               }, 250);
+
+               angular.element('.selected').velocity({
+                  translateY: '88px'
+               },{
+                  duration: 400,
+                  easing: [0.34, 0.34, 0.25, 1]
+               });
             }
         });
 
